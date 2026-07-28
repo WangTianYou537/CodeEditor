f27f090034c89eeff7dd3f52d489a357d3f53bd4
Wantu <raineyaileen674@gmail.com>
2026-07-27 17:57:12 -0900

    fix: fix COmpletionProvider.java

--- src/main/java/cn/wty5/editor/complete/CompletionProvider.java
+++ src/main/java/cn/wty5/editor/complete/CompletionProvider.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeSet;
+import java.util.HashSet;
 
 /**
  * Completion sources driven by a {@link LanguageSpec}: keywords, types,
